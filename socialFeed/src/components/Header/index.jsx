e0ec7527c331cb04485dd3ccc797d28f10f2a7ce
Dev Paolo <paolo.pedro.picinin@gmail.com>
2022-08-31 14:35:06 -0300

    IMPORTANTE - versão anterior quebra a aplicação

--- socialFeed/src/components/Header/index.jsx
+++ socialFeed/src/components/Header/index.jsx
@@ -4,8 +4,8 @@ import socialIcon from '../../assets/icon-svg.svg'
 
 export function Header(){
     return (
-        <header className="header">
-           <img  className="logo" src={socialIcon} alt="socialIcon"/>
+        <header className='header'>
+           <img  className='logo' src={socialIcon} alt="socialIcon"/>
            <strong className='logo-text'>socialFeed</strong>
         </header>
         
